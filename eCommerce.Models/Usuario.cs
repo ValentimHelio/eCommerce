ca7afce11dc84300eb18a9e0366e2c9d4379066d
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-01-29 16:50:59 -0400

    12. S04E04 - Modelo Contato

--- eCommerce.Models/Usuario.cs
+++ eCommerce.Models/Usuario.cs
@@ -9,14 +9,15 @@ namespace eCommerce.Models
     public class Usuario
     {
         public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Sexo { get; set; }
-        public string Rg { get; set; }
-        public string Cpf { get; set; }
-        public string NomeMae { get; set; }
-        public string SituacaoCadastro { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Sexo { get; set; }
+        public string? Rg { get; set; }
+        public string? Cpf { get; set; }
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
         public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }
 
         /*
          * TODO - Vincular com as classes: 
