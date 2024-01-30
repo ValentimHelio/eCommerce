f95c01599547a30fb66c91e2660532e1e0450479
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-01-30 16:57:44 -0400

    17. S04E09 - UsuarioRepository

---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public List<Usuario> _db = new List<Usuario>();
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.Find(x => x.Id == id)!;
+        }
+
+        public void add(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+        public void update(Usuario usuario)
+        {
+            _db.Remove(Get(usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void delete(int id)
+        {
+            _db.Remove(Get(id));
+        }
+
+    }
+}
