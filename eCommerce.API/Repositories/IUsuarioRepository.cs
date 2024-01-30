257813f02981af254698edec772d707447695f0a
Valentim Helio de Oliveira <timvale@hotmail.com>
2024-01-30 16:42:37 -0400

    16. S04E08 - IUsuarioRepository

---
+++ eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Get();
+        Usuario Get(int id);
+        void add(Usuario usuario);
+        void update(Usuario usuario);
+        void delete(int id);
+
+    }
+}
